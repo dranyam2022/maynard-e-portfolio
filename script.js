4a318b1eb332acdd7e8bbdd03d4487e13f78d7cc
dranyam2022 <maynardescalante@gmail.com>
2023-03-07 23:27:26 +0800

    completed mobile nav buttons and modal

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const cards = document.querySelectorAll(".card");
 const navBtn = document.querySelector(".nav__btn");
 const navLinks = document.querySelector(".nav__links");
+const navClose = document.querySelector(".fa-circle-xmark");
+const navOpen = document.querySelector(".fa-bars");
+const modal = document.querySelector(".modal");
 
 function scrollWindowHandler() {
   cards.forEach((card) => {
@@ -23,3 +26,22 @@ scrollWindowHandler();
 
 /* navBtn click handler on mobile screen */
 navBtn.addEventListener("click", clickNavHandler);
+
+function clickNavHandler(evt) {
+  console.log(evt.target.classList.value);
+  if (evt.target.classList.value.includes("fa-bars")) {
+    navOpen.classList.replace("nav--open", "nav--close");
+    modal.style.display = "block";
+  } else {
+    navOpen.classList.replace("nav--close", "nav--open");
+    modal.style.display = "none";
+  }
+}
+
+modal.addEventListener("click", clickNavHandler);
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 1200) {
+    modal.style.display = "none";
+    navOpen.classList.replace("nav--close", "nav--open");
+  }
+});
