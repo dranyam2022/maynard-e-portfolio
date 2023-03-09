95c47d68f9603f140c18285e175674e8e2a5bb9a
dranyam2022 <maynardescalante@gmail.com>
2023-03-09 13:20:51 +0800

    corrected animation timming bug by using setTimeout

--- script.js
+++ script.js
@@ -6,29 +6,26 @@ const navOpen = document.querySelector(".fa-bars");
 const modal = document.querySelector(".modal");
 
 function scrollWindowHandler() {
-  cards.forEach((card) => {
-    const rectTop = card.getBoundingClientRect().top;
-    const rectBottom = card.getBoundingClientRect().bottom;
+  let trigBottom = (window.innerHeight / 5) * 4;
+  let trigTop = window.innerHeight / 5;
+  cards.forEach((card, idx) => {
+    const cardTop = card.getBoundingClientRect().top;
+    const cardBottom = card.getBoundingClientRect().bottom;
 
-    if (window.innerHeight - 90 <= rectTop || rectBottom <= 20) {
-      card.style.opacity = "0.1";
-      card.style.marginTop = "5rem";
+    if (trigBottom <= cardTop || cardBottom <= trigTop) {
+      card.classList.replace("card--show", "card--hidden");
     } else {
-      card.style.opacity = "1";
-      card.style.marginTop = "2rem";
+      card.classList.replace("card--hidden", "card--show");
     }
   });
 }
 
 window.addEventListener("scroll", scrollWindowHandler);
-/* init scrollWindowHandler */
-scrollWindowHandler();
 
 /* navBtn click handler on mobile screen */
 navBtn.addEventListener("click", clickNavHandler);
 
 function clickNavHandler(evt) {
-  console.log(evt.target.classList.value);
   if (evt.target.classList.value.includes("fa-bars")) {
     navOpen.classList.replace("nav--open", "nav--close");
     modal.style.display = "block";
@@ -39,9 +36,9 @@ function clickNavHandler(evt) {
 }
 
 modal.addEventListener("click", clickNavHandler);
+
 window.addEventListener("resize", () => {
-  if (window.innerWidth >= 1200) {
-    modal.style.display = "none";
-    navOpen.classList.replace("nav--close", "nav--open");
-  }
+  setTimeout(scrollWindowHandler, 100);
 });
+
+scrollWindowHandler();
